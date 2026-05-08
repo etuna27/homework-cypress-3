3b9950d13d4fc1e104fb40a6c118db1a4a9878d4
etuna27 <etunanatroshvili7@gmail.com>
2026-05-08 22:47:13 +0400

    change

--- cypress/e2e/new_user.cy.js
+++ cypress/e2e/new_user.cy.js
@@ -6,9 +6,8 @@ describe('Register User Test', () => {
   it('should register successfully', function () {
     cy.visit(this.user.baseUrl);
     cy.contains('Signup / Login').click();
-    const dynamicEmail = `new_${Date.now()}_${this.user.email}`;
+    cy.get('[data-qa="signup-email"]').type(this.user.email); 
     cy.get('[data-qa="signup-name"]').type(this.user.name);
-    cy.get('[data-qa="signup-email"]').type(dynamicEmail);
     cy.get('[data-qa="signup-button"]').click();
     cy.fillRegistrationForm(this.user);
     cy.get('[data-qa="account-created"]').should('be.visible');
